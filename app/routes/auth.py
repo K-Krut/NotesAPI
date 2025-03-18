321d3f6b676792610b825012f4755d9409ec9ee2
K-Krut <katiakrut448@gmail.com>
2025-03-18 16:57:00 +0200

    /api/auth/register endpoint added

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,16 +1,24 @@
+from http.client import HTTPException
 from typing import Any
-from fastapi import APIRouter, Depends
-from schemas.users import UserResponse, User
+from fastapi import APIRouter, Depends, HTTPException
+from app.schemas.users import UserResponse, UserSchema
 from sqlalchemy.orm import Session
-from database import get_db
-
+from app.database import get_db
+from app.crud.users import get_user_by_email, create_user
 
 router = APIRouter()
 
 
 @router.post("/register", response_model=UserResponse)
-def register(user: User, db: Session = Depends(get_db)) -> Any:
-    pass
+def register(user: UserSchema, db: Session = Depends(get_db)) -> Any:
+    check_user = get_user_by_email(db, email=user.email)
+
+    if check_user:
+        raise HTTPException(
+            status_code=400,
+            detail=f"User with '${user.email}' email already registered",
+        )
+    return create_user(db, user)
 
 
 @router.post("/login", response_model=UserResponse)
