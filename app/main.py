6efd25c73c719b383f0e32cb6451b6a09a4ae3bb
K-Krut <katiakrut448@gmail.com>
2025-03-18 17:03:20 +0200

    db initialization added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from app.database import init_db
 from app.routes import auth, notes
 
 app = FastAPI(title="Notes API")
 
+init_db()
 
 app.include_router(notes.router, prefix="/api/notes", tags=["notes"])
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
