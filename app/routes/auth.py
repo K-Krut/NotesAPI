332957d5e95fbc4d4149cf4e8f47a19f936c08ff
K-Krut <katiakrut448@gmail.com>
2025-03-18 17:12:31 +0200

    exceptions handle added in register endpoint

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -11,14 +11,22 @@ router = APIRouter()
 
 @router.post("/register", response_model=UserResponse)
 def register(user: UserSchema, db: Session = Depends(get_db)) -> Any:
-    check_user = get_user_by_email(db, email=user.email)
+    try:
+        check_user = get_user_by_email(db, email=user.email)
 
-    if check_user:
+        if check_user:
+            raise HTTPException(
+                status_code=400,
+                detail=f"User with '${user.email}' email already registered",
+            )
+        return create_user(db, user)
+    except HTTPException as error:
+        raise error
+    except Exception as error:
         raise HTTPException(
-            status_code=400,
-            detail=f"User with '${user.email}' email already registered",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal server error. ${error}",
         )
-    return create_user(db, user)
 
 
 @router.post("/login", response_model=UserResponse)
