67da768cf8c1cba6613dd1536b967bf1d8c5e70b
K-Krut <katiakrut448@gmail.com>
2025-03-19 18:47:43 +0200

    updated note creation response

--- app/routes/notes.py
+++ app/routes/notes.py
@@ -3,8 +3,8 @@ from http.client import HTTPException
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 
 from app.auth.jwt import get_user_by_jwt_token
-from app.crud.notes import create_note_db
-from app.schemas.notes import NoteSchema
+from app.crud.notes import create_note_db, get_note_db
+from app.schemas.notes import NoteSchema, NoteResponse, NoteParentResponse
 from sqlalchemy.orm import Session
 from app.database import get_db
 
@@ -22,10 +22,20 @@ def get_note(note_id: int):
     pass
 
 
-@router.post("/")
+@router.post("/", response_model=NoteResponse)
 def create_note(note: NoteSchema, db: Session = Depends(get_db), user_id: int = Depends(get_user_by_jwt_token)):
     try:
-        return create_note_db(db, note, user_id)
+        note_record = create_note_db(db, note, user_id)
+        note_parent = get_note_db(db, note_record.parent_id)
+
+        return NoteResponse(
+            id=note_record.id,
+            name=note_record.name,
+            details=note_record.details,
+            parent=NoteParentResponse.validate(note_parent) if note_parent else None,
+            created_at=note_record.created_at,
+            updated_at=note_record.updated_at,
+        )
     except Exception as error:
         logger.error(f'----#ERROR in POST /api/notes: {error}')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal server error\n{error}")
