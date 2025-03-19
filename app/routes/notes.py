56b17e40a262a8c453e9450ab61cebd637d653f8
K-Krut <katiakrut448@gmail.com>
2025-03-19 18:10:16 +0200

    added endpoibnt for note creation

--- app/routes/notes.py
+++ app/routes/notes.py
@@ -1,6 +1,15 @@
-from fastapi import APIRouter
+import logging
+from http.client import HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+
+from app.auth.jwt import get_user_by_jwt_token
+from app.crud.notes import create_note_db
+from app.schemas.notes import NoteSchema
+from sqlalchemy.orm import Session
+from app.database import get_db
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.get("/")
@@ -14,8 +23,12 @@ def get_note(note_id: int):
 
 
 @router.post("/")
-def create_note():
-    pass
+def create_note(note: NoteSchema, db: Session = Depends(get_db), user_id: int = Depends(get_user_by_jwt_token)):
+    try:
+        return create_note_db(db, note, user_id)
+    except Exception as error:
+        logger.error(f'----#ERROR in POST /api/notes: {error}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal server error\n{error}")
 
 
 @router.put("/{note_id")
