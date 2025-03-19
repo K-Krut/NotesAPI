cf2f3512eecaf04ce622b2ac2982997b39ed4b57
K-Krut <katiakrut448@gmail.com>
2025-03-19 18:38:18 +0200

    added schema for note Response

--- app/schemas/notes.py
+++ app/schemas/notes.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Optional
 from pydantic import BaseModel
 
@@ -5,6 +6,17 @@ from pydantic import BaseModel
 class NoteSchema(BaseModel):
     name: str
     details: str
-    user_id: int
     parent_id: Optional[int] = None
 
+
+class NoteResponse(BaseModel):
+    id: int
+    name: str
+    details: str
+    parent: Optional["NoteResponse"] = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+        arbitrary_types_allowed = True
