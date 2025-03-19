e6b8027c0abe58724a1a135784922f088841b1b6
K-Krut <katiakrut448@gmail.com>
2025-03-19 18:37:31 +0200

    get note by id get_note_db

--- app/crud/notes.py
+++ app/crud/notes.py
@@ -4,9 +4,13 @@ from app.models.models import Note
 from app.schemas.notes import NoteSchema
 
 
-def create_note_db(db: Session, note: NoteSchema, user_id: int):
+def create_note_db(db: Session, note: NoteSchema, user_id: int) -> Note:
     note_record = Note(**note.model_dump(), user_id=user_id)
     db.add(note_record)
     db.commit()
     db.refresh(note_record)
     return note_record
+
+
+def get_note_db(db: Session, note_id: int) -> Note | None:
+    return db.query(Note).filter(Note.id == note_id).first()
