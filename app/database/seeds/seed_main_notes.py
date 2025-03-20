f1963d8644c916b23b39c40e63f736e99b612b2e
K-Krut <katiakrut448@gmail.com>
2025-03-20 08:49:39 +0200

    added user connection

--- app/database/seeds/seed_main_notes.py
+++ app/database/seeds/seed_main_notes.py
@@ -1,8 +1,10 @@
 import argparse
+import random
+
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.models.models import Note
+from app.models.models import Note, User
 
 from faker import Faker
 from random import randint
@@ -11,10 +13,13 @@ fake = Faker()
 
 
 def seed_main_notes(db: Session, count: int = 10):
+    users = db.query(User).all()
+
     for _ in range(count):
         note = Note(
             name=fake.sentence(nb_words=randint(1, 10)),
             details=fake.text(max_nb_chars=3000),
+            user_id=random.choice([user.id for user in users]),
             created_at=fake.date_time_this_year(),
             updated_at=fake.date_time_this_year(),
         )
@@ -22,6 +27,7 @@ def seed_main_notes(db: Session, count: int = 10):
     db.commit()
 
 
+# python -m app.database.seeds.seed_main_notes --n 20
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="seed db with fake notes")
     parser.add_argument("--n", type=int, default=10, help="notes number")
