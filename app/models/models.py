e198077fec43f45e08ecba145543d462e7616ea5
K-Krut <katiakrut448@gmail.com>
2025-03-21 20:13:02 +0200

    updated user schema for new response

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
--- app/schemas/notes.py
+++ app/schemas/notes.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from typing import Optional, List
 from pydantic import BaseModel
 
