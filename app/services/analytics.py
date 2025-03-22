8f8c37ea0cc74a50f32ba49d8ba913b117601237
K-Krut <katiakrut448@gmail.com>
2025-03-22 15:18:54 +0200

    refactored & optimized

--- app/services/analytics.py
+++ app/services/analytics.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 import nltk
 import string
 
@@ -19,17 +18,16 @@ nltk.download('wordnet')
 CUSTOM_FILTER_SYMBOL = "—–…•«»“”"
 
 
-def filter_token(token):
-    return token not in stopwords.words('english') and token not in string.punctuation and token not in CUSTOM_FILTER_SYMBOL
+def filter_token(token: str) -> bool:
+    return (token not in stopwords.words('english')
+            and token not in string.punctuation and token not in CUSTOM_FILTER_SYMBOL)
 
 
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())
     filtered_tokens = [token for token in tokens if filter_token(token)]
 
-    lemmatized_tokens = [WordNetLemmatizer().lemmatize(token) for token in filtered_tokens]
-
-    return lemmatized_tokens  # return ' '.join(lemmatized_tokens)
+    return [WordNetLemmatizer().lemmatize(token) for token in filtered_tokens]
 
 
 def prepare_notes_for_response(notes_arr: List[dict]) -> List[dict]:
@@ -44,29 +42,26 @@ def join_all_words(notes_arr: List[dict]):
     for record in notes_arr:
         result.extend(record.get('text'))
 
-    return result
+    return [word for record in notes_arr for word in record.get("text")]
+
+
+def get_preprocessed_notes(notes: List[Note]) -> List[dict]:
+    result = []
+    for note in notes:
+        text = preprocess_text(note.details)
+        result.append({
+            "note": note,
+            "text": text,
+            "length": len(text)
+        })
+    return sorted(result, key=lambda x: x.get('length'))
 
 
 def analyze_notes(notes: List[Note]):
-    """
-    You'll also need to create an Analytics feature with an endpoint that
-    analyzes the notes' database.
-    This should calculate various statistics including:
-      - total word count across all notes,
-      - average note length,
-      - most common words or phrases,
-      - and identify the top 3 longest and shortest notes.
-    Use appropriate Python libraries such as NumPy, Pandas, or NLTK for this analysis.
-    """
-    preprocessed_notes = [{"note": note, "text": preprocess_text(note.details)} for note in notes]
-
-    for note in preprocessed_notes:
-        note['length'] = len(note.get("text", []))
-
-    preprocessed_notes = sorted(preprocessed_notes, key=lambda note: note.get('length'))
-    all_words_num = sum([note.get("length") for note in preprocessed_notes])
-    average_note_length = int(all_words_num / len(preprocessed_notes))
+    preprocessed_notes = get_preprocessed_notes(notes)
     all_words = join_all_words(preprocessed_notes)
+    all_words_num = len(all_words)
+    average_note_length = int(all_words_num / len(preprocessed_notes))
 
     return {
         "all_words": all_words_num,
