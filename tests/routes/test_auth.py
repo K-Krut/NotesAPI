f30cb4a7bdda07fc552d75c028d46fd57392f108
K-Krut <katiakrut448@gmail.com>
2025-03-22 18:37:26 +0200

    test_register_with_existing_email

--- tests/routes/test_auth.py
+++ tests/routes/test_auth.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from datetime import timedelta
 
+user = {
+    'email': 'test_email@gmail.com',
+    'password': 'testpassfortests',
+}
+
 
 def test_register(client):
-    user = {
-        'email': 'test_email@gmail.com',
-        'password': 'testpassfortests',
-    }
     response = client.post(
         "/api/auth/register",
         json=user
@@ -18,3 +19,14 @@ def test_register(client):
     assert response_data.get('ai_requests_used') == 0
     assert response_data.get('ai_requests_limit') == 50
     assert response_data.get('ai_requests_reset')[:10] == (datetime.today() + timedelta(days=30)).isoformat()[:10]
+
+
+
+def test_register_with_existing_email(client):
+    response = client.post(
+        "/api/auth/register",
+        json=user
+    )
+    assert response.status_code == 400
+
+
