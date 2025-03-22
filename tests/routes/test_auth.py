d6478d6331727f5528dff9ee8da3372b59b4a693
K-Krut <katiakrut448@gmail.com>
2025-03-22 18:04:00 +0200

    added test for registration

--- tests/routes/test_auth.py
+++ tests/routes/test_auth.py
@@ -1,4 +1,20 @@
+from datetime import datetime
+from datetime import timedelta
 
 
 def test_register(client):
-    pass
+    user = {
+        'email': 'test_email@gmail.com',
+        'password': 'testpassfortests',
+    }
+    response = client.post(
+        "/api/auth/register",
+        json=user
+    )
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert response_data.get('email') == user['email']
+    assert response_data.get('ai_requests_used') == 0
+    assert response_data.get('ai_requests_limit') == 50
+    assert response_data.get('ai_requests_reset')[:10] == (datetime.today() + timedelta(days=30)).isoformat()[:10]
