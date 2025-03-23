6a14bd703f0890b8693772b0364cb954e937e049
K-Krut <katiakrut448@gmail.com>
2025-03-23 23:36:55 +0200

    test_analyze_notes updated & fake notes objs added

--- tests/services/test_analytics.py
+++ tests/services/test_analytics.py
@@ -1,16 +1,13 @@
-def test_analyze_notes_endpoint(client, bulk_create_notes_analytics, test_analytics_user_token):
-    response = client.get(
-        "/api/notes/stats",
-        headers={"Authorization": f"Bearer {test_analytics_user_token}"}
-    )
+from app.services.analytics import analyze_notes
 
-    assert response.status_code == 200
-    response_data = response.json()
 
-    assert response_data.get("all_words")
-    assert response_data.get("average_note_length")
-    assert response_data.get("most_common_words")
-    assert response_data.get("shortest_notes")
-    assert response_data.get("longest_notes")
-    assert len(response_data["shortest_notes"]) <= 3
-    assert len(response_data["longest_notes"]) <= 3
+def test_analyze_notes(fake_analytics_notes):
+    result = analyze_notes(fake_analytics_notes)
+
+    assert result["all_words"]
+    assert result["average_note_length"]
+    assert result["most_common_words"]
+    assert result["shortest_notes"]
+    assert result["longest_notes"]
+    assert len(result["shortest_notes"]) <= 3
+    assert len(result["longest_notes"]) <= 3
