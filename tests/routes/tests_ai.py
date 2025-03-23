10892fbe4e890cbfb1041dff86ff245adced0193
K-Krut <katiakrut448@gmail.com>
2025-03-23 16:49:42 +0200

    refactored auth tests module & added /api/ai/text/summarize for cases with invalid token

--- tests/routes/tests_ai.py
+++ tests/routes/tests_ai.py
@@ -1,8 +1,9 @@
 import pytest
 from tests.data.ai import TEXT_SUMMARIZATION_TEST_DATA
+from tests.data.auth import INVALID_JWT_TOKEN
 
 
-def test_summarize_text_success(client, test_user_token):
+def test_summarize_text(client, test_user_token):
     response = client.post(
         "/api/ai/text/summarize",
         json={"details": TEXT_SUMMARIZATION_TEST_DATA['details']},
@@ -12,3 +13,19 @@ def test_summarize_text_success(client, test_user_token):
     assert response.status_code == 200
     assert response.json().get('summary') == TEXT_SUMMARIZATION_TEST_DATA['summary']
 
+
+def test_summarize_text_without_token(client):
+    response = client.post(
+        "/api/ai/text/summarize",
+        json={"details": TEXT_SUMMARIZATION_TEST_DATA["details"]}
+    )
+    assert response.status_code == 401
+
+
+def test_summarize_text_invalid_token(client):
+    response = client.post(
+        "/api/ai/text/summarize",
+        json={"details": TEXT_SUMMARIZATION_TEST_DATA["details"]},
+        headers={"Authorization": f"Bearer {INVALID_JWT_TOKEN}"}
+    )
+    assert response.status_code == 401
