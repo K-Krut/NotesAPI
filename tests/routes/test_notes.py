44bfbe66bf1374be6276b51619f2e40896299284
K-Krut <katiakrut448@gmail.com>
2025-03-23 23:36:55 +0200

    test_analyze_notes updated & fake notes objs added

--- tests/routes/test_notes.py
+++ tests/routes/test_notes.py
@@ -234,3 +234,21 @@ def test_delete_note_access_denied(client, test_second_user_token, test_user_tok
         headers={"Authorization": f"Bearer {test_second_user_token}"}
     )
     assert response.status_code == 403
+
+
+def test_analyze_notes_endpoint(client, bulk_create_notes_analytics, test_analytics_user_token):
+    response = client.get(
+        "/api/notes/stats",
+        headers={"Authorization": f"Bearer {test_analytics_user_token}"}
+    )
+
+    assert response.status_code == 200
+    response_data = response.json()
+
+    assert response_data.get("all_words")
+    assert response_data.get("average_note_length")
+    assert response_data.get("most_common_words")
+    assert response_data.get("shortest_notes")
+    assert response_data.get("longest_notes")
+    assert len(response_data["shortest_notes"]) <= 3
+    assert len(response_data["longest_notes"]) <= 3
