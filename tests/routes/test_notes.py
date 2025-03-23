15f032dd4047043cdc47bf877d58d000b6a8ef14
K-Krut <katiakrut448@gmail.com>
2025-03-23 16:18:28 +0200

    fixtures refactored

--- tests/routes/test_notes.py
+++ tests/routes/test_notes.py
@@ -1,31 +1,7 @@
 import pytest
 import random
 
-from tests.data.notes import NOTE, NOTES_DATA, NOTE_FULLY_UPDATE, NOTE_VERSION, NOTES_USER, NOTES_USER_2
-
-
-@pytest.fixture(scope="module")
-def test_user_token(client):
-
-    response_register = client.post("/api/auth/register", json=NOTES_USER)
-    assert response_register.status_code == 200
-
-    response = client.post("/api/auth/login", json=NOTES_USER)
-    assert response.status_code == 200
-
-    return response.json().get("access_token")
-
-
-@pytest.fixture(scope="module")
-def test_second_user_token(client):
-
-    response_register = client.post("/api/auth/register", json=NOTES_USER_2)
-    assert response_register.status_code == 200
-
-    response = client.post("/api/auth/login", json=NOTES_USER_2)
-    assert response.status_code == 200
-
-    return response.json().get("access_token")
+from tests.data.notes import NOTE, NOTES_DATA, NOTE_FULLY_UPDATE
 
 
 @pytest.fixture(scope="module")
